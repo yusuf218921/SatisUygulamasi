ce6ffd259f6ef58d0cf9c99140906b3e9c6d5e6d
sinanengin <sinan.engin.02@gmail.com>
2022-12-15 17:53:59 +0300

    İlan oluşturma sistemi yazılacak. CheckPoint Push

--- src/UserMenu.java
+++ src/UserMenu.java
@@ -7,6 +7,19 @@ public class UserMenu extends Main
         {
             System.out.println("\nİyi Günler");
             System.out.println(users.get(loginId).getFirstName() + " " + users.get(loginId).getLastName().toUpperCase());
+
+            System.out.println("\n[1] İlanlara Göz At");
+            System.out.println("[2] İlan Oluştur");
+            System.out.println("[3] İlan Sil");
+            System.out.println("[4] İlanlarım");
+            System.out.println("[5] Kullanıcı Bilgilerini Görüntüle");
+            System.out.println("[6] Kullanıcı Bilgilerini Güncelle");
+            System.out.println("[7] Hesaptan Çıkış Yap");
+            System.out.println("[8] Uygulamadan Çıkış Yap\n");
+            System.out.println("\nSeçiminizi yapınız: ");
+            secim = scanner.nextInt();
+
+
         }
     }
 }
