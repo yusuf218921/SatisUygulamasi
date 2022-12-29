8668b015dfeeff93eca607c2aa85c1eb3e0d2259
Yusuf <yusufsonmez951@gmail.com>
2022-12-30 00:47:04 +0300

    çoğu hata düzeltildi

--- src/Static.java
+++ src/Static.java
@@ -7,11 +7,6 @@ public class Static {
     static ArrayList<Account> accounts = new ArrayList<>();
     static ArrayList<Ilan> ilanlar = new ArrayList<>();
     static ArrayList<User> users = new ArrayList<>();
-    static ArrayList<Otomobil> otomobiller = new ArrayList<>();
-    static ArrayList<Motosiklet> motosikletler = new ArrayList<>();
-    static ArrayList<Daire> daireler = new ArrayList<>();
-    static ArrayList<Arsa> arsalar = new ArrayList<>();
-
     static int secim = -1;
     static int loginId;
     static int ilanKayitId=0;
